23d4a4e3893b02122410b00684f4cf82ef39c601
ebar4426 <ebar4426@fullsail.edu>
2014-02-11 16:36:47 -0600

    finished the excercise

--- Expressions_worksheet/js/script.js
+++ Expressions_worksheet/js/script.js
@@ -21,5 +21,23 @@ console.log("Each person ate " + slicesPerPerson + " slices of pizza at the part
 var slices = 8;//number of pizza per box
 var people = 13;//number of people at the party
 var boxes = 4;//boxes of pizza ordered
-var sparkySlices = (boxes * slices) % people;//the formula to solve for distribution of slices
+var sparkySlices = (boxes * slices) % people;//the formula to solve for sparkys slices
 console.log("Sparky got " + sparkySlices + " slices of pizza");//answer
+
+//Average shopping bill
+
+var shopTotals = [165, 145, 96, 160, 120];
+var totalSpent = shopTotals[0] + shopTotals[1] + shopTotals[2] + shopTotals[3] + shopTotals[4];
+var average = totalSpent/5;
+console.log("You have spent a total of $" + totalSpent + " on groceries over 5 weeks. That is an average of $" + average + "per week");
+
+
+//Discounts
+
+var videoGame = 59.99;
+var discount = .30;
+var game = "Video Game";
+var tax = 1.0825;
+var totalNoTax = videoGame - (videoGame * discount);
+var totalandtax = totalNoTax * tax;
+console.log("Your " + game + " originally $" + videoGame + ", but after a 30% discount, is now $" + totalNoTax + " without tax, and $" + totalandtax + " with tax");
