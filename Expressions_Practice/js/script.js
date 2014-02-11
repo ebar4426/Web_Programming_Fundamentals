8dbcb57ff4320b904962d324731d92a8329edbb7
ebar4426 <ebar4426@fullsail.edu>
2014-02-11 03:47:09 -0600

    array expresions

--- Expressions_Practice/js/script.js
+++ Expressions_Practice/js/script.js
@@ -1,11 +1,6 @@
 // Javascript
 
-var quiz1 = 85;
-var quiz2 = 95;
-var quiz3 = 100;
-var quiz4 = 85;
+var orangeBins = [234, 567, 883];
 
-// the average of the items is the sum of the items divided by the number of items
-
-var average = (quiz1 + quiz2 + quiz3 + quiz4)/4;
-console.log(average);
+var total = orangeBins[0] + orangeBins[1] + orangeBins[2];
+console.log(total)
