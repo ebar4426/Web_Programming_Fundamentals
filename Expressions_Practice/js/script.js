d614b773be699171b69de88c557aecbf88a8b4eb
ebar4426 <ebar4426@fullsail.edu>
2014-02-11 03:21:47 -0600

    expressions math operators

--- Expressions_Practice/js/script.js
+++ Expressions_Practice/js/script.js
@@ -1,5 +1,8 @@
 // Javascript
 
-var yearBorn = 1992;
-var age = 2013 - yearBorn;
-console.log(age);
+// area of triangle is half of the width times the height
+
+var width = 8;
+var height = 7;
+var area = width * height/2;
+console.log(area);
