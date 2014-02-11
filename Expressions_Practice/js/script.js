ad26cde67e553dc64c5c3d3b6000dc8a3fc45c9b
ebar4426 <ebar4426@fullsail.edu>
2014-02-11 04:09:15 -0600

    assinment operators

--- Expressions_Practice/js/script.js
+++ Expressions_Practice/js/script.js
@@ -1,8 +1,5 @@
 // Javascript
 
-var areaCode = 345;
-var firstPart = 344;
-var secPart = 4455;
-
-var phoneNo = "(" + String(areaCode) + ")" + String(firstPart) + "-" +  String(secPart);
-console.log(phoneNo)
+var a = 3;
+a += 4;
+console.log(a);
