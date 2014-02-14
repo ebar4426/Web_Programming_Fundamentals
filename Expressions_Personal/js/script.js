6e6f7b641ea07eac592aeff40242b4fae62b8e7a
ebar4426 <ebar4426@fullsail.edu>
2014-02-13 20:31:12 -0600

    adding in formula

--- Expressions_Personal/js/script.js
+++ Expressions_Personal/js/script.js
@@ -1,8 +1,10 @@
 // Personal Expressions File
 
 //Tis code is used to figure out how much time each week has been spent on homework.
-var numClass = prompt("How many classes are you curently enrolled in?")//asked to gather number of classes
-var time = prompt("How much time do you spend on homework for each class that you have?")//asked to find out how much time spent doing homework for each class
-var days = prompt("How many days a week do you spend doing homework.")//asked to find out how many days a week is spent doing homework
+var numClass = prompt("How many classes are you curently enrolled in?");//asked to gather number of classes
+var time = prompt("How much time do you spend on homework for each class that you have?");//asked to find out how much time spent doing homework for each class
+var days = prompt("How many days a week do you spend doing homework.");//asked to find out how many days a week is spent doing homework
+var week = 7;
+var total = (numClass * time * days)/week;
 
 
