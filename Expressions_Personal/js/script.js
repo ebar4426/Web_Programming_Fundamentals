de412596b749e67d325cc5d06bee8313fbb620b6
ebar4426 <ebar4426@fullsail.edu>
2014-02-13 20:37:34 -0600

    fixed an error on verification

--- Expressions_Personal/js/script.js
+++ Expressions_Personal/js/script.js
@@ -6,6 +6,6 @@ var time = prompt("How much time do you spend on homework for each class that yo
 var days = prompt("How many days a week do you spend doing homework.");//asked to find out how many days a week is spent doing homework
 var week = 7;//this is the constant for the number of days in a week
 var total = (numClass * time * days)/week;//this is the fomrula to solve the problem and find out how many hours on average of each day 
-alert("You spend an average of " + total + "hr/day each week that you are in shcool!")
-console.log("You spend an average of " + total + "hr/day each week that you are in shcool!")
+alert("You spend an average of " + total + "hr/day each week that you are in shcool!");
+console.log("You spend an average of " + total + "hr/day each week that you are in shcool!");
 
