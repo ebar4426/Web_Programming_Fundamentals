1d22751efd5c6e2ec811d4a335e145590076a91d
ebar4426 <ebar4426@fullsail.edu>
2014-02-13 20:39:22 -0600

    fixed some comments

--- Expressions_Personal/js/script.js
+++ Expressions_Personal/js/script.js
@@ -1,6 +1,6 @@
 // Personal Expressions File
 
-//Tis code is used to figure out how much time each week has been spent on homework.
+//Tis code is used to figure out how much time you weould speend each day on homework if you did it each day.
 var numClass = prompt("How many classes are you curently enrolled in?");//asked to gather number of classes
 var time = prompt("How much time do you spend on homework for each class that you have?");//asked to find out how much time spent doing homework for each class
 var days = prompt("How many days a week do you spend doing homework.");//asked to find out how many days a week is spent doing homework
