8a2959500a0f794eaccf0de9a2e7b60106cb66d1
ebar4426 <ebar4426@fullsail.edu>
2014-02-13 20:55:49 -0600

    added the array

--- Expressions_Industry/js/script.js
+++ Expressions_Industry/js/script.js
@@ -1,3 +1,5 @@
 // This is the Industry Expressions page...
 
 //On this page we will demonstrate arrays and find out how they can help in mathmatical calculations. In this expression we will be calculating the amout of time spent developing a web site. In this example we will only be doing the hours in one week. And we will then find out how much the contractor must pay you the developer for your work.
+
+var hoursWorked = [8, 7, 9, 6, 4, 6, 11];
