25f73ad2797d17d6cb475b807f318e8961e62ee4
ebar4426 <ebar4426@fullsail.edu>
2014-02-13 21:23:20 -0600

    added coments and fixed html code

--- Expressions_Wacky/js/script.js
+++ Expressions_Wacky/js/script.js
@@ -1,1 +1,4 @@
-alert("This is in the js file");
+//This is my Wacky page
+
+//In this expression I though it would be fun to calculate the amount of sugar consumed by drinking coke or a soft drink in one year.
+
