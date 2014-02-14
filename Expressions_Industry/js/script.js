acbae6d64d7418fc39c6babb9b05165a1f38f076
ebar4426 <ebar4426@fullsail.edu>
2014-02-13 21:05:35 -0600

    added fomrulas

--- Expressions_Industry/js/script.js
+++ Expressions_Industry/js/script.js
@@ -4,3 +4,5 @@
 
 var hoursWorked = [8, 7, 9, 6, 4, 6, 11];//Here please enter the number of hours worked this week. This is to find out how many hours have been worked in total
 var payPerHour = 30;//please enter your pay an hour that your employer is paying you.
+var totalPay = (hoursWorked[0] + hoursWorked[1] + hoursWorked[2] + hoursWorked[3] + hoursWorked[4] + hoursWorked[5] + hoursWorked[6])* payPerHour;//this is the formula to find out how much your employer needs to pay you 
+var averagePerDay = totalPay / 7;//this is used to find out the average amount of money made each day.
