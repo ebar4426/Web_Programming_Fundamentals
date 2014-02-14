490e69d812eeae6824760470ca98653ff984015c
ebar4426 <ebar4426@fullsail.edu>
2014-02-13 20:35:37 -0600

    added comments

--- Expressions_Personal/js/script.js
+++ Expressions_Personal/js/script.js
@@ -4,7 +4,7 @@
 var numClass = prompt("How many classes are you curently enrolled in?");//asked to gather number of classes
 var time = prompt("How much time do you spend on homework for each class that you have?");//asked to find out how much time spent doing homework for each class
 var days = prompt("How many days a week do you spend doing homework.");//asked to find out how many days a week is spent doing homework
-var week = 7;
+var week = 7;//this is the constant for the number of days in a week
 var total = (numClass * time * days)/week;
 alert("You spend an average of " + total + "hr/day each week that you are in shcool!")
 console.log("You spend an average of " + total + "hr/day each week that you are in shcool!")
