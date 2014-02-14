5a297f4d821560558d716d2daa287eaa19dec786
ebar4426 <ebar4426@fullsail.edu>
2014-02-13 21:29:47 -0600

    added prompt variables

--- Expressions_Wacky/js/script.js
+++ Expressions_Wacky/js/script.js
@@ -2,3 +2,5 @@
 
 //In this expression I though it would be fun to calculate the amount of sugar consumed by drinking coke or a soft drink in one year.
 
+var sizeCan = prompt("What size of can/bottle/cup do you usualy drink when consuming a coke or soda? (ex. 12oz 16oz 32oz.....)")
+var cansPerDay = prompt("How many cans/bottles/cups of that size do you drink each day you deside to drink a soda or coke?")
