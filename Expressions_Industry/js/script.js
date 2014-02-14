26599b7229e886226d3bde5a9ae3c0df041245de
ebar4426 <ebar4426@fullsail.edu>
2014-02-13 20:54:01 -0600

    added explanation of expression

--- Expressions_Industry/js/script.js
+++ Expressions_Industry/js/script.js
@@ -1,1 +1,3 @@
-alert("This is in the js file");
+// This is the Industry Expressions page...
+
+//On this page we will demonstrate arrays and find out how they can help in mathmatical calculations. In this expression we will be calculating the amout of time spent developing a web site. In this example we will only be doing the hours in one week. And we will then find out how much the contractor must pay you the developer for your work.
