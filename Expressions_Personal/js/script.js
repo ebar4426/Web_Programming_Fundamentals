a4d1242bb4949cff944aa6d4e725c65bc9d9362c
ebar4426 <ebar4426@fullsail.edu>
2014-02-13 20:27:29 -0600

    added variabls

--- Expressions_Personal/js/script.js
+++ Expressions_Personal/js/script.js
@@ -1,4 +1,8 @@
 // Personal Expressions File
 
 //Tis code is used to figure out how much time each week has been spent on homework.
-var numClass = prompt("How many classes are you curently enrolled in?")
+var numClass = prompt("How many classes are you curently enrolled in?")//asked to gather number of classes
+var time = prompt("How much time do you spend on homework for each class that you have?")
+var days = prompt("How many days a week do you spend doing homework.")
+
+
