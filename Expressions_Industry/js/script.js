b44c05b66219b30cd13dbed4e0d6f7f7cef31838
ebar4426 <ebar4426@fullsail.edu>
2014-02-13 20:58:58 -0600

    added more variables

--- Expressions_Industry/js/script.js
+++ Expressions_Industry/js/script.js
@@ -2,4 +2,5 @@
 
 //On this page we will demonstrate arrays and find out how they can help in mathmatical calculations. In this expression we will be calculating the amout of time spent developing a web site. In this example we will only be doing the hours in one week. And we will then find out how much the contractor must pay you the developer for your work.
 
-var hoursWorked = [8, 7, 9, 6, 4, 6, 11];
+var hoursWorked = [8, 7, 9, 6, 4, 6, 11];//Here please enter the number of hours worked this week. This is to find out how many hours have been worked in total
+var payPerHour = 30;//please enter your pay an hour that your employer is paying you.
