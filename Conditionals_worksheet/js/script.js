08949f26fd58c56533a55c7701ee956d23ec1a56
ebar4426 <ebar4426@fullsail.edu>
2014-02-20 01:19:58 -0600

    first example

--- Conditionals_worksheet/js/script.js
+++ Conditionals_worksheet/js/script.js
@@ -1,1 +1,11 @@
 //Conditionals Work Sheet
+//Stuff your face 1
+var compWeight = 265;//weight of the actual competetor
+var qualWeight = 250;//qualifing weight to enter competion
+if(compWeight >= qualWeight){//equasion on how to find out if competitor can qualifi
+	console.log("The competitor qualifies for the heavyweight division.")//console message
+}else{
+	console.log("The competitor needs to gain some weight!")//message if under weight
+}
+//Group 1: Expressions with Conditionals
+//
