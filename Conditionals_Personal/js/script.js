419ad4c69f0fae38e6d6dae99869d96777b25c1f
ebar4426 <ebar4426@fullsail.edu>
2014-02-20 05:44:46 -0600

    added comments

--- Conditionals_Personal/js/script.js
+++ Conditionals_Personal/js/script.js
@@ -4,7 +4,7 @@
 var priceOfItems = [25, 12, 30, 15, 22, 26, 40];//please add the price of your items
 var discountCard = .15;//please add the precent of your member ship card discount if you dont have one please put 0
 var totalPre = priceOfItems[0] + priceOfItems[1] + priceOfItems[2] + priceOfItems[3] + priceOfItems[4] + priceOfItems[5] + priceOfItems[6];//formula to find out price before tax.
-var discount;
+var discount;//discount total
 var total;
 if(discountCard >= .001){
 	discount = totalPre * discountCard;
