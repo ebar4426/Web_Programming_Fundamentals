6bf3f5b173b8f844cf8e698165a1de65159ccbfe
ebar4426 <ebar4426@fullsail.edu>
2014-02-20 06:16:33 -0600

    added comments

--- Conditionals_Industry/js/script.js
+++ Conditionals_Industry/js/script.js
@@ -1,8 +1,8 @@
 //Eric Barrera 2/19/2014 Conditionals Assignment
 //This is my industry page where I will be Creating a PreLaunch check list for a web page.
-var firstAns = prompt("We are going to go over a Pre Launch check list for your web page. Have you reviewed your Spelling, Gramer and Punctuation? If yes please type yes, if not please type no.");
-var secAns;
-var thirAns;
+var firstAns = prompt("We are going to go over a Pre Launch check list for your web page. Have you reviewed your Spelling, Gramer and Punctuation? If yes please type yes, if not please type no.");//for first prompt and answer
+var secAns;//for second answer
+var thirAns;//for third answer
 var forthAns;
 if(firstAns === "yes"){
 	secAns = prompt("Ok! Great! Now have you checked the Compatibility with other web browsers? If yes please type yes, if not please type no.");
@@ -11,7 +11,7 @@ if(firstAns === "yes"){
 		if(thirAns === "yes"){
 			forthAns = prompt("Almost done! Last but certainly not the least. Have you Validated your web site? If yes please type yes, if not please type no.");
 			if (forthAns = "yes"){
-				alert("Awsome!! Your site is ready for launch all major parts have been looked over! Good Luck!!");
+				alert("Awesome!! Your site is ready for launch all major parts have been looked over! Good Luck!!");
 			}else{
 				alert("Please make sure you look over this step. Its important to double or triple check your site before launch.");
 			}
