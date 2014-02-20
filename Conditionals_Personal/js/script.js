0726674a1c2fede13a205e512e9248a045d85c94
ebar4426 <ebar4426@fullsail.edu>
2014-02-20 05:44:00 -0600

    fix code works

--- Conditionals_Personal/js/script.js
+++ Conditionals_Personal/js/script.js
@@ -2,11 +2,11 @@
 //This is my personal conditional code.
 //This calculator will determin the price of of your grocerys with with a discount card or without and with sales tax 
 var priceOfItems = [25, 12, 30, 15, 22, 26, 40];//please add the price of your items
-var discountCard = [.15];//please add the precent of your member ship card discount if you dont have one please put 0
+var discountCard = .15;//please add the precent of your member ship card discount if you dont have one please put 0
 var totalPre = priceOfItems[0] + priceOfItems[1] + priceOfItems[2] + priceOfItems[3] + priceOfItems[4] + priceOfItems[5] + priceOfItems[6];
 var discount;
 var total;
-if(discountCard > 0){
+if(discountCard >= .001){
 	discount = totalPre * discountCard;
 	total = (totalPre - discount)* 1.0825;//total with discount and tax
 	console.log("Your total before tax and your discount card was $" + totalPre + " your total with your discounts and tax is $" + total + " you saved $" + discount + "!");
