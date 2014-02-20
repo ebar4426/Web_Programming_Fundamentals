57f7fff2dd8cd4962b6343c9a93a34545839e0a2
ebar4426 <ebar4426@fullsail.edu>
2014-02-19 22:36:07 -0600

    practice on conditionals

--- Conditionals_Practice/js/script.js
+++ Conditionals_Practice/js/script.js
@@ -1,1 +1,9 @@
-alert("This is in the js file");
+//conditionals Practice
+
+var oldEnough = true;
+
+//if child is old enough. print to the console "you can ride!"
+if(oldEnough){
+	//code performed if condition is true.
+	console.log("You can ride the coaster!");
+}
