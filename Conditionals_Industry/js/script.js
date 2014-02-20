e3666edbb432cf066facf0c49f2cba3502e98e0d
ebar4426 <ebar4426@fullsail.edu>
2014-02-20 05:57:30 -0600

    added code

--- Conditionals_Industry/js/script.js
+++ Conditionals_Industry/js/script.js
@@ -1,2 +1,3 @@
 //Eric Barrera 2/19/2014 Conditionals Assignment
 //This is my industry page where I will be Creating a PreLaunch check list for a web page.
+var firstAns = prompt("We are going to go over a Pre Launch check list for your web page.")
