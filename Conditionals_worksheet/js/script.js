b2fc41282e41425326928fdb4b1819ff8feb18cb
ebar4426 <ebar4426@fullsail.edu>
2014-02-20 01:44:33 -0600

    finnished group one calculator for temp conversion works great!

--- Conditionals_worksheet/js/script.js
+++ Conditionals_worksheet/js/script.js
@@ -3,9 +3,22 @@
 var compWeight = 265;//weight of the actual competetor
 var qualWeight = 250;//qualifing weight to enter competion
 if(compWeight >= qualWeight){//equasion on how to find out if competitor can qualifi
-	console.log("The competitor qualifies for the heavyweight division.")//console message
+	console.log("The competitor qualifies for the heavyweight division.");//console message
 }else{
-	console.log("The competitor needs to gain some weight!")//message if under weight
+	console.log("The competitor needs to gain some weight!");//message if under weight
 }
 //Group 1: Expressions with Conditionals
-//
+//Celsius to Fahrenheit converter
+
+var degrees = 100;//the number you want to convert 
+var unit = "c";//the unit of temprrature 
+var total;//answer
+if(unit === "f"){//to determin if a Celsius conversion
+	total = (degrees - 32)*5/9;//formula for celsius
+	console.log("The temperature is " + total + " degrees Celsius.");//console answer
+}else if(unit === "c"){//to determin if a Fahrenheit conversion
+	total = degrees * 9/5 + 32;//formula for fahrenheit
+	console.log("The temperature is " + total + " degrees Fahrenheit.");//console answer
+}
+// you can enter any of the data set to find out the answers here they are 32F is 0C 100C is 212F 90F is 32.22C
+// gives correct answer every time just make sure to change the unit of the temp you are trying to convert
