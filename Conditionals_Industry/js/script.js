5c3b84ecff89a0db05868801e4a9be555f20c29e
ebar4426 <ebar4426@fullsail.edu>
2014-02-20 06:01:47 -0600

    added more code

--- Conditionals_Industry/js/script.js
+++ Conditionals_Industry/js/script.js
@@ -1,3 +1,9 @@
 //Eric Barrera 2/19/2014 Conditionals Assignment
 //This is my industry page where I will be Creating a PreLaunch check list for a web page.
-var firstAns = prompt("We are going to go over a Pre Launch check list for your web page.")
+var firstAns = prompt("We are going to go over a Pre Launch check list for your web page. Have you reviewed your Spelling, Gramer and Punctuation? If yes please type yes, if not please type no.");
+var secAns;
+var thirAns;
+var forthAns;
+if(firstAns === "yes"){
+	secAns = prompt("Ok! Great!")
+}
