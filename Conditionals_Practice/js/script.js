cf5380a172276c7db9324cd4a61d881edffba2b1
ebar4426 <ebar4426@fullsail.edu>
2014-02-19 22:47:39 -0600

    more practice

--- Conditionals_Practice/js/script.js
+++ Conditionals_Practice/js/script.js
@@ -3,7 +3,10 @@
 var kidHeight = 47;
 var minHeight = 48;
 //if child is old enough. print to the console "you can ride!"
-if(kidHeight + 2 > minHeight){
+if(kidHeight > minHeight){
 	//code performed if condition is true.
 	console.log("You can ride the coaster!");
+}else{
+	//code performed if condition is false.
+	console.log("Sorry kid you've got some growing to do!");
 }
