1ccf43e5a4cb36846d60b681fdc2eb341fa64a5a
ebar4426 <ebar4426@fullsail.edu>
2014-02-20 05:44:28 -0600

    added comment

--- Conditionals_Personal/js/script.js
+++ Conditionals_Personal/js/script.js
@@ -3,7 +3,7 @@
 //This calculator will determin the price of of your grocerys with with a discount card or without and with sales tax 
 var priceOfItems = [25, 12, 30, 15, 22, 26, 40];//please add the price of your items
 var discountCard = .15;//please add the precent of your member ship card discount if you dont have one please put 0
-var totalPre = priceOfItems[0] + priceOfItems[1] + priceOfItems[2] + priceOfItems[3] + priceOfItems[4] + priceOfItems[5] + priceOfItems[6];
+var totalPre = priceOfItems[0] + priceOfItems[1] + priceOfItems[2] + priceOfItems[3] + priceOfItems[4] + priceOfItems[5] + priceOfItems[6];//formula to find out price before tax.
 var discount;
 var total;
 if(discountCard >= .001){
