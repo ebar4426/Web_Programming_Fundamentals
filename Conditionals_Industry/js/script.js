1904e7e91ec235728b8e64b869fd15f3d620b4c4
ebar4426 <ebar4426@fullsail.edu>
2014-02-20 06:09:58 -0600

    added more code

--- Conditionals_Industry/js/script.js
+++ Conditionals_Industry/js/script.js
@@ -5,5 +5,11 @@ var secAns;
 var thirAns;
 var forthAns;
 if(firstAns === "yes"){
-	secAns = prompt("Ok! Great!")
+	secAns = prompt("Ok! Great! Now have you checked the Compatibility with other web browsers? If yes please type yes, if not please type no.");
+	if(secAns === "yes"){
+		thirAns = prompt("Good! Ok have you Checked and made sure that your images are optimized and displaying properly? If yes please type yes, if not please type no.");
+		if(thirAns === "yes"){
+			forthAns = prompt("Almost done! Last but certainly not the least. Have you Validated your web site?")
+		}
+	}
 }
