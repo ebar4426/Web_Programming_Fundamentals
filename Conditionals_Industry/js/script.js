38115dbe61aeaf34f28193fb4e292bb8c8fc2652
ebar4426 <ebar4426@fullsail.edu>
2014-02-20 06:18:04 -0600

    added comments

--- Conditionals_Industry/js/script.js
+++ Conditionals_Industry/js/script.js
@@ -3,11 +3,11 @@
 var firstAns = prompt("We are going to go over a Pre Launch check list for your web page. Have you reviewed your Spelling, Gramer and Punctuation? If yes please type yes, if not please type no.");//for first prompt and answer
 var secAns;//for second answer
 var thirAns;//for third answer
-var forthAns;
-if(firstAns === "yes"){
-	secAns = prompt("Ok! Great! Now have you checked the Compatibility with other web browsers? If yes please type yes, if not please type no.");
-	if(secAns === "yes"){
-		thirAns = prompt("Good! Ok have you Checked and made sure that your images are optimized and displaying properly? If yes please type yes, if not please type no.");
+var forthAns;//for fourth answer
+if(firstAns === "yes"){//to make sure this step has been taken
+	secAns = prompt("Ok! Great! Now have you checked the Compatibility with other web browsers? If yes please type yes, if not please type no.");//prompt to next question if previous answer was yes
+	if(secAns === "yes"){//to make sure secound answer was yes
+		thirAns = prompt("Good! Ok have you Checked and made sure that your images are optimized and displaying properly? If yes please type yes, if not please type no.");//prompt for 
 		if(thirAns === "yes"){
 			forthAns = prompt("Almost done! Last but certainly not the least. Have you Validated your web site? If yes please type yes, if not please type no.");
 			if (forthAns = "yes"){
