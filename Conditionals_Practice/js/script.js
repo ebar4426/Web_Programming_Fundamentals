c83846fd836c6c7955f4bd13bb08fbe3776510d5
ebar4426 <ebar4426@fullsail.edu>
2014-02-20 00:39:56 -0600

    practice

--- Conditionals_Practice/js/script.js
+++ Conditionals_Practice/js/script.js
@@ -1,16 +1,43 @@
 //conditionals Practice
 
-var kidHeight = 52;
-var minHeight = 48;
-var parentHeight = 45; //Height of kid when parent needed to ride.
+//var kidHeight = 52;
+//var minHeight = 48;
+//var parentHeight = 45; //Height of kid when parent needed to ride.
 //if child is old enough. print to the console "you can ride!"
-if(kidHeight > minHeight){
+//if(kidHeight > minHeight){
 	//code performed if condition is true.
-	console.log("You can ride the coaster!");
-}else if(kidHeight > parentHeight){
+	//console.log("You can ride the coaster!");
+//}else if(kidHeight > parentHeight){
 	//you can ride with a parent present.
-	console.log("You can ride but only with a parent present.");
-}else{
+	//console.log("You can ride but only with a parent present.");
+//}else{
 	//sorry kid you have some growing to do.
-	console.log("Sorry kid you've got some growing to do!");
+	//console.log("Sorry kid you've got some growing to do!");
+//}
+// logical operators
+
+var budget = 300;
+var iPhonePrice = 199.99;
+var paycheck = 200;
+
+if(iPhonePrice < budget && paycheck > 300){
+	console.log("We can buy the phone!");
+}else{
+	colsole.log("No phone for you!!");
 }
+if(paycheck > 300){
+	console.log("We can buy the phone!");
+}else{
+	colsole.log("No phone for you!!");
+}
+
+
+var age = 11;
+var book;
+//if kid is under 10 they get green egs and ham otherwise they get the time machine 
+if(age < 10){
+	book = "Green Eggs and Ham";
+}else{
+	book = "The Time Machine";
+}
+console.log(book);
