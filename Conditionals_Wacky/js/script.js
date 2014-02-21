c3ce9dc734629e14eb56d9ada35aa55044fe5b17
ebar4426 <ebar4426@fullsail.edu>
2014-02-20 20:22:56 -0600

    added intro and fixed files

--- Conditionals_Wacky/js/script.js
+++ Conditionals_Wacky/js/script.js
@@ -1,1 +1,9 @@
-alert("This is in the js file");
+//Eric Barrera 2/19/2014 Conditionals Assignment
+//this is my wacky code. it is a storry where you make the dessisions.
+var firstDesision;
+var secondDesision;
+var thirdDesision;
+var badEnd;
+var okEnd;
+var goodEnd;
+alert("This is a desision making story! Lets begin, to set the location you arive at a dark and spooky house that you have to inspect. There are lots of rumors about this place. You are a reporter thats never been scared of a challange. You want to get the scoop on this place, it could make your carrer. Your boss gave you a deadline of tomorrow. Wanting the best time for paranormal activity you deside to go tonight! You have your head camara and headset to document all your adventures. To make matters worse, a huge storm is rolling in. You've been given a key by the owners of the property. You open the front door and step inside......");
