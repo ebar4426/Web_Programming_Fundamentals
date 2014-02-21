620060f8a2544609efe6b3b91c3a3d7a6357fdff
ebar4426 <ebar4426@fullsail.edu>
2014-02-20 20:39:06 -0600

    added code

--- Conditionals_Wacky/js/script.js
+++ Conditionals_Wacky/js/script.js
@@ -6,4 +6,12 @@ var thirdDesision;
 var badEnd;
 var okEnd;
 var goodEnd;
-alert("This is a desision making story! Lets begin, to set the location you arive at a dark and spooky house that you have to inspect. There are lots of rumors about this place. You are a reporter thats never been scared of a challange. You want to get the scoop on this place, it could make your carrer. Your boss gave you a deadline of tomorrow. Wanting the best time for paranormal activity you deside to go tonight! You have your head camara and headset to document all your adventures. To make matters worse, a huge storm is rolling in. You've been given a key by the owners of the property. You open the front door and step inside......");
+alert("This is a decision making story! Lets begin, to set the location you arrive at a dark and spooky house that you have to inspect. There are lots of rumors about this place. You are a reporter that’s never been scared of a challenge. You want to get the scoop on this place, it could make your carrer. Your boss gave you a deadline of tomorrow. Wanting the best time for paranormal activity you decide to go tonight! You have your head camera and headset to document all your adventures. To make matters worse, a huge storm is rolling in. You've been given a key by the owners of the property. You open the front door and step inside......");
+firstDesision = prompt("You step inside a very dusty entrance. You take up your options. You can go up stairs or can leave right now before it gets bad. Make your desision. Type 1 to go upstairs, Type 2 to go out front door.");
+if(firstDesision === "1"){
+	secondDesision = prompt("You walk up stairs and see a shadow race to an open room, your camera picks up movement and you document what you saw by speaking into the headset. Your options are, The open door room, The room with a closed door or take evidence and leave. Type 1 for open room. Type 2 for closed room. Type 3 for Take evidence and leave.");
+	if(secondDesision === "1"){
+		
+	}
+}
+
