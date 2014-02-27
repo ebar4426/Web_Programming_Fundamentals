d16bbfc79e25a476ba8ba79a93679f432ad57467
ebar4426 <ebar4426@fullsail.edu>
2014-02-27 02:00:24 -0600

    adding code

--- Functions_worksheet/js/script.js
+++ Functions_worksheet/js/script.js
@@ -1,1 +1,7 @@
 //Functions Worksheet
+//Circumference problem
+
+var circumCalc = function(radius){//This is the function to call out this calculation
+	var pi = 3.14159265359;//this is the value of pi
+	total = 2 * pi * radius;
+};
