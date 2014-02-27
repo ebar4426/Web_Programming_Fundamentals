70d58ca8454be33bf62ad4fb6408bc26204eaf97
ebar4426 <ebar4426@fullsail.edu>
2014-02-27 02:18:01 -0600

    finised commenting

--- Functions_worksheet/js/script.js
+++ Functions_worksheet/js/script.js
@@ -12,10 +12,10 @@ console.log("The circumference of the circle is " + circle + ".");//this is how 
 
 //Stung! Problem
 
-var beeCalc = function(weight){
-	var stingAmount = 8.6666666667;
-	var totalStings = stingAmount * weight;
-	return totalStings;
+var beeCalc = function(weight){//this is the function to call out the calculation
+	var stingAmount = 8.6666666667;//this is the given info for amount of bee stings it takes to kill an animal per pound
+	var totalStings = stingAmount * weight;//this is the formula to find out how many bee stings in total it would take
+	return totalStings;//this is to return the info
 };
-var stings = beeCalc(120);
-console.log("It takes " + stings + " bee stings to kill this animal.");
+var stings = beeCalc(120);//Please enter the weight trying to be calculated here to find out how many bee stings it would take to kill
+console.log("It takes " + stings + " bee stings to kill this animal.");//console message that is dispayed after calculation is compleate
