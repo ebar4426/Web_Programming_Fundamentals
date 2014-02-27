625f825ff08ab573c46807451058e8c6dd68c666
ebar4426 <ebar4426@fullsail.edu>
2014-02-27 03:31:02 -0600

    code start

--- Functions_Personal/js/script.js
+++ Functions_Personal/js/script.js
@@ -1,3 +1,16 @@
 //Personal Functions 
 //Eric Barrera 2/26/2014
 //This is a functions page that will be used to dertermin the size of dipers your chile should use based on weight and the type of formula that should be used by age
+var babyCalc = function(weight, age){//used to call out calculation....weight should be in lb and age should be in months
+	if(weight > 5 && weight < 15){
+		var stageOne = "Size 1 ";
+		return stageOne;
+	}
+	if(age > 2 && age < 7){
+		var foodOne = "Stage 1 ";
+		return foodOne;
+	}
+};
+var fomula = babyCalc(7, 0);
+var dipar = babyCalc(0, 5);
+console.log(fomula + dipar);
