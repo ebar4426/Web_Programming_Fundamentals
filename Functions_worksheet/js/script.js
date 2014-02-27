48146d0701c2d0c012d0e4e7939d8fc61f8028f3
ebar4426 <ebar4426@fullsail.edu>
2014-02-27 02:09:50 -0600

    quick fix

--- Functions_worksheet/js/script.js
+++ Functions_worksheet/js/script.js
@@ -4,7 +4,7 @@
 var circumCalc = function(radius){//This is the function to call out this calculation
 	var pi = 3.14159265359;//this is the value of pi
 	total = 2 * pi * radius;//this is the formula to calculate the circumference of a circle
-	return total;
+	return total;//this is to return the data
 };
-var circle = circumCalc(7);
-console.log(circle);
+var circle = circumCalc(7);//Please enter the radius in the () for a print out in web console of the total circumference of a circle
+console.log("The circumference of the circle is " + circle + ".");//this is how the console prints out
