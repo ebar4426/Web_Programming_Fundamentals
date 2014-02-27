0864881031d360557b85e95730dcd4454f9b734b
ebar4426 <ebar4426@fullsail.edu>
2014-02-27 02:14:56 -0600

    code test

--- Functions_worksheet/js/script.js
+++ Functions_worksheet/js/script.js
@@ -8,3 +8,14 @@ var circumCalc = function(radius){//This is the function to call out this calcul
 };
 var circle = circumCalc(7);//Please enter the radius in the () for a print out in web console of the total circumference of a circle
 console.log("The circumference of the circle is " + circle + ".");//this is how the console prints out
+
+
+//Stung! Problem
+
+var beeCalc = function(weight){
+	var stingAmount = 8.6666666667;
+	var totalStings = stingAmount * weight;
+	return totalStings;
+};
+var stings = beeCalc(120);
+console.log("It takes " + stings + " bee stings to kill this animal.");
