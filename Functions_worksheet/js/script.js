46b54c58b5150b72e003df5d041f279ec6dba0f8
ebar4426 <ebar4426@fullsail.edu>
2014-02-27 02:07:24 -0600

    finished with circle function

--- Functions_worksheet/js/script.js
+++ Functions_worksheet/js/script.js
@@ -3,5 +3,8 @@
 
 var circumCalc = function(radius){//This is the function to call out this calculation
 	var pi = 3.14159265359;//this is the value of pi
-	total = 2 * pi * radius;
+	total = 2 * pi * radius;//this is the formula to calculate the circumference of a circle
+	return total;
 };
+var circle = circumCalc(7);
+console.log(circle);
