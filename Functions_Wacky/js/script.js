f8b052adfef75135db6048af9f2edb23bec248b6
ebar4426 <ebar4426@fullsail.edu>
2014-02-27 21:28:44 -0600

    finnished code works great

--- Functions_Wacky/js/script.js
+++ Functions_Wacky/js/script.js
@@ -5,4 +5,8 @@ var wordCalc = function(wordsPMin, hoursTypeEssay, averageNumOfLetters){
 	var min = 60;
 	var total = wordsPMin * min * hoursTypeEssay;//how to get the total per hour your typing and find out how much you would type if you typed an essay
 	var strokes = total * averageNumOfLetters;//please enter the average number of leters of the words your using in your essay
+	var fin = "The total amount of words typed if you spent " + hoursTypeEssay + " hours writing your essay, you would have typed " + total + " words. That's about " + strokes + " key strokes if your words average out to " + averageNumOfLetters + ".";//aswer for calculator
+	return fin;//return data
 };
+var finAnswer = wordCalc(35, 3, 5);//enter your words per min, then your hours working on an essay, then the average amount of letters in the words used in your essay
+console.log(finAnswer);//used to print to the console the final answer
