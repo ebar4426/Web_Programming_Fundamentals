a6c7682dd8827a75ed21b4ebe00f11bb48494d12
ebar4426 <ebar4426@fullsail.edu>
2014-02-27 21:05:44 -0600

    added comments

--- Functions_Industry/js/script.js
+++ Functions_Industry/js/script.js
@@ -2,11 +2,11 @@
 //Industry function
 //This calculator will help you understand how much bandwith you will need for your network and calculate how fast your computer is reciving data.
 //Before you start using the calculator please find out the speed of your network or server and find out how fast your computer is reciving data using your terminal by sending packets to that particular network or server and how many useres will be using it at the same time.
-function bandCalc(networkBand, localBpsToNet, numberOfUsers){
-	var bpsFirst = 1000000;
-	var conver = 125000;
+function bandCalc(networkBand, localBpsToNet, numberOfUsers){//here are the arguments for the user to put in
+	var bpsFirst = 1000000;//conversion from Mbps to bps
+	var conver = 125000;//conversion back from Bps to Mbps
 	var bpsSecond = (networkBand * bpsFirst)/ 8;//this is for finding out the Bps of the network or aplication thats hosting your computer
-	var howManyCan = bpsSecond / localBpsToNet;
+	var howManyCan = bpsSecond / localBpsToNet;//formula to find out how many people can use the server at one time
 	var yourAppSpeed = localBpsToNet / 125000;
 	if(numberOfUsers < howManyCan){
 		var answer = "The amount of people that can use this application or server at " + networkBand + "Mbps are " + howManyCan + " using your computers utilazation at " + yourAppSpeed + "Mbps.";
